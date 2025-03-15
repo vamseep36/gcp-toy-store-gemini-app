f895fa3d8543fa3e6c99a874f87fc980300159ce
Vishnu Vamsee Paningipalli <62656098+vamseep36@users.noreply.github.com>
2025-03-15 20:22:54 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,8 @@
-
+gcloud services enable  alloydb.googleapis.com \
+  compute.googleapis.com \
+  cloudresourcemanager.googleapis.com \
+  servicenetworking.googleapis.com \
+  run.googleapis.com \
+  cloudbuild.googleapis.com \
+  cloudfunctions.googleapis.com \
+  aiplatform.googleapis.com
