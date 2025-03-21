c06e342c0270cc860b6e6dc284e6ac5d9c3b5982
Vishnu Vamsee Paningipalli <62656098+vamseep36@users.noreply.github.com>
2025-03-21 20:42:48 +0530

    Update complete.sh

--- scripts/complete.sh
+++ scripts/complete.sh
@@ -1,1 +1,11 @@
+gcloud alloydb clusters create vector-cluster \
+    --region=us-central1 \
+    --network=default \
+    --database-version=POSTGRES_15 \
+    --password=alloydb
 
+gcloud alloydb instances create vector-instance \
+    --cluster=vector-cluster \
+    --region=us-central1 \
+    --instance-type=PRIMARY \
+    --cpu-count=4
